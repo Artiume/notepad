079bad43fe6288cde85d8d037747031d4e784afe
Dante Issaias <dante@issaias.net>
2019-12-26 22:38:24 +0000

    chore(eslint): disable import/no-named-as-default
    Signed-off-by: Dante Issaias <dante@issaias.net>

--- ui/lib/login.ts
+++ ui/lib/login.ts
@@ -1,10 +1,8 @@
 import fetch from "isomorphic-unfetch"
 
-const isServer = typeof window === "undefined"
-
 export const login = (email: string, password: string) =>
   fetch("/api/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password })
+    body: JSON.stringify({ email, password }),
   })
