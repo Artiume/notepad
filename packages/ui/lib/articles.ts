03b8c00739530655483da8beb6eed6ba76c2a9ba
Dante <dante@issaias.net>
2020-01-01 21:16:33 +0000

    feat(ui/lib): create article helper

--- packages/ui/lib/articles.ts
+++ packages/ui/lib/articles.ts
@@ -1,5 +1,14 @@
 import fetch from "isomorphic-unfetch"
+import Cookies from "js-cookie"
 
 const isServer = typeof window === "undefined"
 
 export const articles = () => fetch(isServer ? "http://localhost:3000/api/articles" : "/api/articles")
+export const createArticle = (title: string, content: string) =>
+  fetch("/api/articles", {
+    body: JSON.stringify({ title, content }),
+    headers: {
+      authorization: Cookies.get("token") ?? "",
+      "Content-Type": "application/json",
+    },
+  })
